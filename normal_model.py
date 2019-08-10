46f79990741d8933750f56fbac48f4b26f85e5c5
Udaikaran Singh <www.udaikaran@gmail.com>
2019-08-10 12:15:49 -0700

    initial commit

---
+++ normal_model.py
@@ -0,0 +1,59 @@
+import torch as torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Generator(nn.Module):
+    
+    def __init__(self, batch_size, latent_space):
+        super(Generator, self).__init__()
+        
+        #storing variables
+        self.latent_space = latent_space
+        self.batch_size = batch_size
+        
+        #architecture
+        self.fully_connected = nn.Linear(in_features = self.latent_space, out_features = 8192)
+        self.conv1 = nn.Conv2d(in_channels= 8, out_channels= 16, kernel_size= 3, stride= 1)
+        self.conv2 = nn.Conv2d(in_channels= 16, out_channels= 16, kernel_size= 3, stride= 1)
+        self.conv3 = nn.Conv2d(in_channels= 16, out_channels= 1, kernel_size= 1, stride= 1)
+    def forward(self, z):
+        # input: z has the shape (batch size x latent space size)
+        # output: (batch size x 3 x 28 x 28) 
+        out = self.fully_connected(z)
+        out = out.view((self.batch_size, 8, 32, 32))
+        out = F.relu(self.conv1(out))
+        out = F.relu(self.conv2(out))
+        out = torch.sigmoid(self.conv3(out))
+        
+        return out
+
+class Discriminator(nn.Module):
+    
+    def __init__(self, batch_size, latent_space):
+        super(Discriminator, self).__init__()
+        
+        #storing variables
+        self.latent_space = latent_space
+        self.batch_size = batch_size
+        
+        #architecture
+        self.conv1 = nn.Conv2d(in_channels= 1, out_channels= 16, kernel_size= 3, stride= 2)
+        self.conv2 = nn.Conv2d(in_channels= 16, out_channels= 32, kernel_size= 3, stride= 2)
+        self.conv3 = nn.Conv2d(in_channels= 32, out_channels= 64, kernel_size= 3, stride= 2)
+        
+        self.fully_connected1 = nn.Linear(in_features= 256, out_features= 128)
+        self.fully_connected2 = nn.Linear(in_features= 128, out_features= 1)
+    
+    def forward(self, x):
+        # input: x has a shape (batch size x 3 x 28 x 28)
+        # output: size is (batch size x 1 x 1 x 1)
+        
+        out = F.relu(self.conv1(x))
+        out = F.relu(self.conv2(out))
+        out = F.relu(self.conv3(out))
+        
+        out = out.view((self.batch_size, 256))
+        out = F.relu(self.fully_connected1(out))
+        out = F.relu(self.fully_connected2(out))
+        
+        return out
---
+++ train_conditional.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+from torch.autograd import Variable
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import random
+import torchvision.transforms as transforms
+
+
+#######################################
+# Importing and setting up networks 
+#######################################
+
+from conditional_model import Generator, Discriminator
+
+cuda_available = torch.cuda.is_available()
+latent_size = 50
+batch_size = 16
+num_classes = 10
+
+if cuda_available:
+    print("Using GPU")
+else:
+    print("Using CPU")
+
+if cuda_available:
+	generator = Generator(batch_size, latent_size, num_classes).cuda()
+	discriminator = Discriminator(batch_size, latent_size, num_classes).cuda()
+
+	if os.path.exists("generator_model_conditional.pth"):
+		print("Loading in Models")
+		generator.load_state_dict(torch.load("generator_model_conditional.pth"))
+		discriminator.load_state_dict(torch.load("discriminator_model_conditional.pth"))
+else:
+	generator = Generator(batch_size, latent_size, num_classes)
+	discriminator = Discriminator(batch_size, latent_size, num_classes)
+
+	if os.path.exists("generator_model_conditional.pth"):
+		print("Loading in Models")
+		generator.load_state_dict(torch.load("generator_model_conditional.pth"))
+		discriminator.load_state_dict(torch.load("discriminator_model_conditional.pth"))
+
+##################################################
+# Definining Hyperparameters of Training Procedure
+##################################################
+
+random.seed(1)
+learning_rate = 0.2e-4
+beta1 = 0.5
+beta2 = 0.999
+num_epochs = 10
+epsilon = 1e-8
+
+optimizer_G = torch.optim.Adam(generator.parameters(), lr= learning_rate, betas= (beta1, beta2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr= learning_rate, betas= (beta1, beta2))
+
+transform = transforms.Compose([transforms.Resize((28, 28)),
+	transforms.ToTensor(), 
+	transforms.Normalize([0.5], [0.5])
+])
+
+os.makedirs("../../data/mnist", exist_ok=True)
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../../data/mnist",
+        train=True,
+        download=True,
+        transform=transform,
+    ),
+    batch_size=batch_size,
+    shuffle=True,
+)
+
+adversarial_loss = torch.nn.BCELoss()
+
+##################################################
+# Training Procedure
+##################################################
+emb = nn.Embedding(num_classes, num_classes) 
+emb.weight.data = torch.eye(num_classes)
+
+Tensor = torch.cuda.FloatTensor if cuda_available else torch.FloatTensor
+
+loss_tracker = []
+
+for epoch in range(num_epochs):
+
+	count = 0
+
+	print("epoch: " + str(epoch + 1))
+
+	for i, (imgs, labels) in enumerate(dataloader):
+
+		valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+		fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
+
+		########################
+        # Generator Step
+        ########################
+
+		real_imgs = Variable(imgs.type(Tensor))
+
+		generator.zero_grad()
+
+		z = Variable(Tensor(torch.rand(batch_size, latent_size)))
+		gen_imgs = generator(z, labels)
+
+		g_loss = adversarial_loss(discriminator(gen_imgs, labels), valid)
+
+		g_loss.backward()
+		optimizer_G.step()
+
+		########################
+		# Discriminator Step
+		########################
+
+		discriminator.zero_grad()
+
+		real_loss = adversarial_loss(discriminator(real_imgs, labels), valid)
+		fake_loss = adversarial_loss(discriminator(gen_imgs.detach(), labels), fake)
+		d_loss = (real_loss + fake_loss) / 2
+
+		d_loss.backward()
+		optimizer_D.step()
+
+	path = "generator_model_conditional.pth"
+	torch.save(generator.state_dict(), path)
+	path = "discriminator_model_conditional.pth"
+	torch.save(discriminator.state_dict(), path)
+
